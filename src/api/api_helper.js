165c07c71414e589d5b8f229d3472220019e633f
Warren <jwarrencedro@MacBook-Pro.local>
2020-08-13 02:41:07 +0800

    api set to heroku

--- src/api/api_helper.js
+++ src/api/api_helper.js
@@ -1,7 +1,7 @@
 
 const ApiHelper = {
-    base_url: 'http://127.0.0.1:8000/',
-    //base_url: 'https://test-bookmarker.herokuapp.com/',
+    //base_url: 'http://127.0.0.1:8000/',
+    base_url: 'https://test-bookmarker.herokuapp.com/',
 }
 
 export default ApiHelper
